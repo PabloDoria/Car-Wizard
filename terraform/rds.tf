fc0785f5fd4ff2000b332f3202fe1d41a4641475
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-01 21:56:37 -0600

    RDS changes to improve reading

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,10 +1,45 @@
+resource "aws_db_subnet_group" "rds_subnet_group" {
+    name        = "rds-subnet-group"
+    subnet_ids  = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id] 
+    description = "Subnet group for RDS"
+}
+
+resource "aws_security_group" "rds_sg" {
+    name_prefix = "rds-sg-"
+    description = "Security Group for RDS"
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        description = "Allow MySQL traffic from ECS"
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        security_groups = [aws_security_group.ecs_sg.id] # Permitir solo desde ECS
+    }
+
+    egress {
+        description = "Allow outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "rds-security-group"
+    }
+}
+
 resource "aws_db_instance" "rds" {
+    identifier           = "my-rds-instance"
+    db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
     allocated_storage    = 20
     engine              = "mysql"
     engine_version      = "8.0"
-    instance_class      = "db.t3.micro"
+    instance_class      = "db.t3.small"
     db_name             = var.rds_db_name
     username           = var.rds_username
     password           = var.rds_password
     skip_final_snapshot = true
+    vpc_security_group_ids = [aws_security_group.rds_sg.id] # Asegura que RDS tenga acceso
 }

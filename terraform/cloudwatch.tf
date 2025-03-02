00e9bb1033cd8717a0fb476409ab7ac09f63e5e6
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-01 19:22:18 -0600

    Docker

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,7 +1,12 @@
 resource "aws_cloudwatch_log_group" "ecs_logs" {
-    name = "/ecs/car-wizard"
-    retention_in_days = 30
+    name              = "/ecs/car-wizard"
+    retention_in_days = 7
+
     lifecycle {
-        prevent_destroy = true
+        ignore_changes = [name]  
+    }
+
+    tags = {
+        Name = "ecs-logs"
     }
 }
--- terraform/networking.tf
+++ terraform/networking.tf
@@ -48,3 +48,34 @@ resource "aws_security_group" "alb_sg" {
         Name = "alb-security-group"
     }
 }
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.vpc.id
+
+    tags = {
+        Name = "internet-gateway"
+    }
+}
+
+resource "aws_route_table" "public_rt" {
+    vpc_id = aws_vpc.vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+
+    tags = {
+        Name = "public-route-table"
+    }
+}
+
+resource "aws_route_table_association" "subnet_1_association" {
+    subnet_id      = aws_subnet.subnet_1.id
+    route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet_2_association" {
+    subnet_id      = aws_subnet.subnet_2.id
+    route_table_id = aws_route_table.public_rt.id
+}

a5fc2e12b2c094a1f2a23a3a87aee474ae4153e7
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-01 18:17:03 -0600

    Docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,3 @@
 terraform {
-        required_version = ">= 1.0.0"
+    required_version = ">= 1.0.0"
 }
-
-    # Llamando a los recursos definidos en otros archivos
-    module "ecr" {
-        source = "./ecr.tf"
-    }
-
-    module "ecs" {
-        source = "./ecs.tf"
-    }
-
-    module "rds" {
-        source = "./rds.tf"
-    }
-
-    module "lambda" {
-        source = "./lambda.tf"
-    }
-
-    module "networking" {
-        source = "./networking.tf"
-    }
-
-    module "cloudwatch" {
-        source = "./cloudwatch.tf"
-    }
-
-    module "alb" {
-        source = "./alb.tf"
-    }

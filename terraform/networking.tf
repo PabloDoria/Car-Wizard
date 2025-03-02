94b40f470529bf1049dcbe87d410b37cd7546254
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-02 00:14:08 -0600

    Cambios de subredes

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -8,10 +8,26 @@ resource "aws_subnet" "subnet_1" {
     availability_zone       = "us-east-1a"
     map_public_ip_on_launch = true
 }
+
+
 resource "aws_subnet" "subnet_2" {
     vpc_id                  = aws_vpc.vpc.id
     cidr_block              = "10.0.2.0/24"
     availability_zone       = "us-east-1b"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_3" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.3.0/24"
+    availability_zone       = "us-east-1c"
+    map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_4" {
+    vpc_id                  = aws_vpc.vpc.id
+    cidr_block              = "10.0.4.0/24"
+    availability_zone       = "us-east-1d"
     map_public_ip_on_launch = true
 }
 
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "rds" {
     allocated_storage    = 20
     engine              = "mysql"
     engine_version      = "8.0"
-    instance_class      = "db.t3.small"
+    instance_class      = "db.t3.micro"
     db_name             = var.rds_db_name
     username           = var.rds_username
     password           = var.rds_password

cf3d579689d2a6cc0a344cb19ac6803b4792913b
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-01 22:12:27 -0600

    Changing networking definition

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,39 +1,14 @@
-resource "aws_vpc" "vpc" {
-    cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "subnet_1" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.0.1.0/24"
-    availability_zone       = "us-east-1a"
-    map_public_ip_on_launch = true
-}
-resource "aws_subnet" "subnet_2" {
-    vpc_id                  = aws_vpc.vpc.id
-    cidr_block              = "10.0.2.0/24"
-    availability_zone       = "us-east-1b"
-    map_public_ip_on_launch = true
-}
-
-resource "aws_security_group" "alb_sg" {
-    name_prefix = "alb-sg-"
-    description = "Security Group for ALB"
+resource "aws_security_group" "rds_sg" {
+    name_prefix = "rds-sg-"
+    description = "Security Group for RDS"
     vpc_id      = aws_vpc.vpc.id
 
     ingress {
-        description = "Allow HTTP traffic from anywhere"
-        from_port   = 80
-        to_port     = 80
+        description = "Allow MySQL traffic from ECS"
+        from_port   = 3306
+        to_port     = 3306
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    ingress {
-        description = "Allow HTTPS traffic from anywhere"
-        from_port   = 443
-        to_port     = 443
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.ecs_sg.id] # Permitir solo desde ECS
     }
 
     egress {
@@ -45,37 +20,40 @@ resource "aws_security_group" "alb_sg" {
     }
 
     tags = {
-        Name = "alb-security-group"
+        Name = "rds-security-group"
     }
 }
 
-resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.vpc.id
+resource "aws_security_group" "ecs_sg" {
+    name        = "ecs-security-group"
+    description = "Allow traffic from ALB and ECS tasks"
+    vpc_id      = aws_vpc.vpc.id
+
+    ingress {
+        description = "Allow traffic from ALB"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        security_groups = [aws_security_group.alb_sg.id]
+    }
+
+    ingress {
+        description = "Allow traffic from ECS to RDS"
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        security_groups = [aws_security_group.rds_sg.id]
+    }
+
+    egress {
+        description = "Allow all outbound traffic"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 
     tags = {
-        Name = "internet-gateway"
+        Name = "ecs-security-group"
     }
-}
-
-resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.vpc.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
-    }
-
-    tags = {
-        Name = "public-route-table"
-    }
-}
-
-resource "aws_route_table_association" "subnet_1_association" {
-    subnet_id      = aws_subnet.subnet_1.id
-    route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "subnet_2_association" {
-    subnet_id      = aws_subnet.subnet_2.id
-    route_table_id = aws_route_table.public_rt.id
 }
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -4,32 +4,6 @@ resource "aws_db_subnet_group" "rds_subnet_group" {
     description = "Subnet group for RDS"
 }
 
-resource "aws_security_group" "rds_sg" {
-    name_prefix = "rds-sg-"
-    description = "Security Group for RDS"
-    vpc_id      = aws_vpc.vpc.id
-
-    ingress {
-        description = "Allow MySQL traffic from ECS"
-        from_port   = 3306
-        to_port     = 3306
-        protocol    = "tcp"
-        security_groups = [aws_security_group.ecs_sg.id] # Permitir solo desde ECS
-    }
-
-    egress {
-        description = "Allow outbound traffic"
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    tags = {
-        Name = "rds-security-group"
-    }
-}
-
 resource "aws_db_instance" "rds" {
     identifier           = "my-rds-instance"
     db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name

c80326211efff8ee5d14973fc3a8bbd5ebefd02f
Pablo Torres Doria <pablotorresdoria@Gmail.com>
2025-03-02 12:36:00 -0600

    Cambios a proyecto PHP

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -20,11 +20,22 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_execution_ecr_policy" {
+    role       = aws_iam_role.ecs_execution_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_logs_policy" {
+    role       = aws_iam_role.ecs_execution_role.name
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+
 resource "aws_ecs_task_definition" "ecs_task" {
     family                   = "car-wizard-task"
     network_mode             = "awsvpc"
     requires_compatibilities = ["FARGATE"]
-    execution_role_arn       = aws_iam_role.ecs_execution_role.arn  # Agregamos estop
+    execution_role_arn       = aws_iam_role.ecs_execution_role.arn 
     cpu                      = "256"
     memory                   = "512"
     
@@ -46,4 +57,24 @@ resource "aws_ecs_task_definition" "ecs_task" {
         }
         }
     ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+    name            = "car-wizard-service"
+    cluster         = aws_ecs_cluster.ecs_cluster.id
+    task_definition = aws_ecs_task_definition.ecs_task.arn
+    desired_count   = 1
+    launch_type     = "FARGATE"
+
+    network_configuration {
+        subnets          = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+        security_groups  = [aws_security_group.alb_sg.id]
+        assign_public_ip = true
+    }
+
+    load_balancer {
+        target_group_arn = aws_lb_target_group.alb_target.arn
+        container_name   = "car-wizard-container"
+        container_port   = 80
+    }
 }
